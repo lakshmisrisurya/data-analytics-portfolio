aa7f47d2c3a75df8c73c4b9cf08edee9dc34512f
Lakshmi Surya <lakshmibicog@gmail.com>
2026-03-09 15:30:28 -0400

    Create stg_requisitions.sql for requisition data query

---
+++ people-analytics-workforce-insights/stg_requisitions.sql
@@ -0,0 +1,14 @@
+SELECT
+    requisition_id,
+    department,
+    role_title,
+    opened_date,
+    closed_date,
+    accepted_offer_date,
+    candidate_hired_date,
+    status,
+    recruiter_name,
+    hiring_manager_id,
+    offers_extended,
+    offers_accepted
+FROM recruiting_requisitions
